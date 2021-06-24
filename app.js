1cc0ec3c8581d6cae50b075dadd67e6553233ddc
Hachi <67706415+hachi-ops@users.noreply.github.com>
2021-06-24 23:58:50 +0100

    Add files via upload
    toggle on mobile

---
+++ app.js
@@ -0,0 +1,21 @@
+// const socials = document.querySelector(" .open-share");
+// const shareBtn = document.getElementById("share");
+// const removeShareBtn = document.querySelector("#remove-share");
+
+// shareBtn.addEventListener("click", () => {
+//     socials.classList.toggle("clicked")
+// })
+
+// removeShareBtn.addEventListener("click", () => {
+//     socials.classList.remove("clicked")
+// })
+
+
+function myFunction() {
+    var x = document.getElementById("myDiv");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
